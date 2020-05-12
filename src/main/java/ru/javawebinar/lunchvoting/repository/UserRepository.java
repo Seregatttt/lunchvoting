388e945ed6b59ae6d0707565a84a02d02deb1ffa
seregatttt <seregatttt@mail.ru>
2020-05-12 20:02:04 +0300

    + repository datajpa

---
+++ src/main/java/ru/javawebinar/lunchvoting/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.lunchvoting.repository;
+
+import ru.javawebinar.lunchvoting.model.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    // null if not found, when updated
+    User save(User user);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    User get(int id);
+
+    // null if not found
+    User getByEmail(String email);
+
+    List<User> getAll();
+
+    default User getWithLunch(int id) {
+        throw new UnsupportedOperationException();
+    }
+}
