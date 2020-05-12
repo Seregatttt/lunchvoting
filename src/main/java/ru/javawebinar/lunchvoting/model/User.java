e89912501baa7e547d0722a28c2f01873c1d0e72
seregatttt <seregatttt@mail.ru>
2020-05-12 19:55:30 +0300

    add Hibernate+ Spring core

--- src/main/java/ru/javawebinar/lunchvoting/model/User.java
+++ src/main/java/ru/javawebinar/lunchvoting/model/User.java
@@ -1,20 +1,74 @@
 package ru.javawebinar.lunchvoting.model;
 
+//import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.util.CollectionUtils;
+
+import javax.persistence.*;
+//import javax.validation.constraints.Email;
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.Set;
 
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@NamedQueries({
+        @NamedQuery(name = User.DELETE, query = "DELETE FROM User u WHERE u.id=:id"),
+        @NamedQuery(name = User.BY_EMAIL, query = "SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1"),
+        @NamedQuery(name = User.ALL_SORTED, query = "SELECT u FROM User u ORDER BY u.name, u.email"),
+})
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "email", name = "users_unique_email_idx")})
 public class User {
+    public static final int START_USERS_SEQ = 100;
 
+    @Id
+    @SequenceGenerator(name = "global_seq_users", sequenceName = "global_seq_users", allocationSize = 1, initialValue = START_USERS_SEQ)
+    //    @Column(name = "id", unique = true, nullable = false, columnDefinition = "integer default nextval('global_seq')")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq_users")
     protected Integer id;
+//  See https://hibernate.atlassian.net/browse/HHH-3718 and https://hibernate.atlassian.net/browse/HHH-12034
+//  Proxy initialization when accessing its identifier managed now by JPA_PROXY_COMPLIANCE setting
 
+ //   @NotBlank
+  //  @Size(min = 2, max = 100)
+    @Column(name = "name", nullable = false)
     protected String name;
 
+    public static final String DELETE = "User.delete";
+    public static final String BY_EMAIL = "User.getByEmail";
+    public static final String ALL_SORTED = "User.getAllSorted";
+
+    @Column(name = "email", nullable = false, unique = true)
+ //   @Email
+  //  @NotBlank
+  //  @Size(max = 100)
     private String email;
 
+    @Column(name = "password", nullable = false)
+  //  @NotBlank
+   // @Size(min = 5, max = 100)
+    // https://stackoverflow.com/a/12505165/548473
+   // @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+            uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role"}, name = "user_roles_unique_idx")})
+    @Column(name = "role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    @BatchSize(size = 200)
     private Set<Role> roles;
+
+//    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")//, cascade = CascadeType.REMOVE, orphanRemoval = true)
+//    @OrderBy("dateTime DESC")
+////    @JsonIgnore
+//    private List<Meal> meals;
 
     public User() {
     }
@@ -73,8 +127,12 @@ public class User {
     }
 
     public void setRoles(Collection<Role> roles) {
-        this.roles = roles.isEmpty() ? EnumSet.noneOf(Role.class) : EnumSet.copyOf(roles);
+        this.roles = CollectionUtils.isEmpty(roles) ? EnumSet.noneOf(Role.class) : EnumSet.copyOf(roles);
     }
+
+//    public List<Meal> getMeals() {
+//        return meals;
+//    }
 
     @Override
     public boolean equals(Object o) {
